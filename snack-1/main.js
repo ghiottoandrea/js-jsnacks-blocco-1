d7a9813be00ec9ab1e79f00f267d933a054cdc27
ghiottoandrea <andreaghiotto9@gmail.com>
2024-02-23 12:14:33 +0100

    fix snack

--- snack-1/main.js
+++ snack-1/main.js
@@ -1,7 +1,10 @@
 //inserisci due numeri tramite prompt
 let number1 = prompt("Inserisci il primo numero");
+console.log(number1);
 let number2 = prompt("Inserisci il secondo numero");
+console.log(number2);
 
+//Confronto per trovare il maggiore e lo stampo
 if (number1 > number2){
     console.log(number1);
     alert("Il numero più grande è "+number1)
