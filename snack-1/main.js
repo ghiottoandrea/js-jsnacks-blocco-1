b78f058d41b045470bcf7006bda63647e71073e6
ghiottoandrea <andreaghiotto9@gmail.com>
2024-02-23 12:12:52 +0100

    first commit

---
+++ snack-1/main.js
@@ -0,0 +1,12 @@
+//inserisci due numeri tramite prompt
+let number1 = prompt("Inserisci il primo numero");
+let number2 = prompt("Inserisci il secondo numero");
+
+if (number1 > number2){
+    console.log(number1);
+    alert("Il numero più grande è "+number1)
+}else{
+    console.log(number2);
+    alert("Il numero più grande è "+number2)
+
+}
