ab9aa492a165d041569e8d0f63d357fe387db9a2
ghiottoandrea <andreaghiotto9@gmail.com>
2024-02-26 10:58:21 +0100

    fix snack 3

--- snack-3/main.js
+++ snack-3/main.js
@@ -1,8 +1,8 @@
 sum = 0
 
  for (let i = 0; i < 10; i++){
-     let number = +(prompt("Inserisci un numero"));
-    sum = sum + number
+     let numbers = Number(prompt("Inserisci un numero"));
+    sum = sum + numbers
 
 }
 
@@ -10,8 +10,8 @@ console.log(sum);
 
 let i = 0;
 while (i < 10) {
-    let number = +(prompt("Inserisci un numero"));
-    sum = sum + number
+    let numbers = Number(prompt("Inserisci un numero"));
+    sum = sum + numbers
 
     i++
 }
